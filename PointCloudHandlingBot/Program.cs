9d914d5c238630167b8285245c05d743f3d03296
VtyurinaSI <sveta@WIN-QF9MU8QVJJQ>
2025-05-22 15:23:44 +0300

    Добавьте файлы проекта.

---
+++ PointCloudHandlingBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PointCloudHandlingBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
