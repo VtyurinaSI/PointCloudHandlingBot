407711faec4a6dc31f6affa7c88f8866deb7709d
VtyurinaSI <sveta@WIN-QF9MU8QVJJQ>
2025-05-22 15:34:51 +0300

    Первичная структура

--- PointCloudHandlingBot/Program.cs
+++ PointCloudHandlingBot/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace PointCloudHandlingBot
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace PointCloudHandlingBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var cts = new CancellationTokenSource();
+            var botClient = new TelegramBotClient(BotToken.token);
+            var handler = new UpdateHandler();
+            try
+            {
+                var me = await botClient.GetMe(cts.Token);
+
+                Console.WriteLine($"{me.FirstName} запущен!");
+                var receiverOptions = new ReceiverOptions
+                {
+                    AllowedUpdates = [UpdateType.Message],
+                    DropPendingUpdates = true
+                };
+
+                handler.OnHandleUpdateStarted += StartHandling;
+                handler.OnHandleUpdateCompleted += CompletedHandling;
+                botClient.StartReceiving(handler.HandleUpdateAsync, handler.HandleErrorAsync, receiverOptions);
+
+
+                Console.WriteLine($"Нажмите клавишу A для выхода (в латинской раскладке)");
+                while (!cts.IsCancellationRequested)
+                {
+                    if (Console.ReadKey().KeyChar == 'A')
+                        await cts.CancelAsync();
+                    else Console.WriteLine($"\nИнформация о боте:\nID: {me.Id}\nUsername: @{me.Username}\nИмя: {me.FirstName} {me.LastName}");
+                }
+            }
+            finally
+            {
+                handler.OnHandleUpdateStarted -= StartHandling;
+                handler.OnHandleUpdateCompleted -= CompletedHandling;
+                cts.Dispose();
+            }
         }
+        private static async Task StartHandling(string msg) => await Task.Run(() => Console.WriteLine($"Началась обработка сообщения \"{msg}\""));
+        private static async Task CompletedHandling(string msg) => await Task.Run(() => Console.WriteLine($"Закончилась обработка сообщения \"{msg}\""));
     }
 }
+
---
+++ PointCloudHandlingBot/UpdateHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using static Telegram.Bot.TelegramBotClient;
+
+namespace PointCloudHandlingBot
+{
+    class UpdateHandler : IUpdateHandler
+    {
+        record CatFactDto(string Fact, int Length);
+        public delegate Task MessageHandler(string msg);
+        public event MessageHandler? OnHandleUpdateStarted;
+        public event MessageHandler? OnHandleUpdateCompleted;
+        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            await Task.Run(() => Console.WriteLine($"Ошибка: {exception.Message}"), cancellationToken);
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+
+            if (update.Message is null)
+            {
+                await Task.Run(() => Console.WriteLine("Message is null"));
+                return;
+            }
+            OnHandleUpdateStarted?.Invoke(update.Message.Text);
+            var botText = "Сообщение получено!";
+            await botClient.SendMessage(update.Message.Chat.Id, botText, cancellationToken: token);
+
+            OnHandleUpdateCompleted?.Invoke(update.Message.Text);
+        }
+
+
+    }
+}
